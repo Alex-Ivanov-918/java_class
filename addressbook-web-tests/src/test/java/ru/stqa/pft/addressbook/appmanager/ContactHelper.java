bbbab5f1daeb761e2263266d792d80d72ab9094b
aivanou <AIvanou@itsupportme.com>
2019-01-09 16:18:01 +0300

    ContactModificationTests creation

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -22,7 +22,7 @@ public class ContactHelper extends HelperBase {
     click(By.xpath("(//input[@name='submit'])[2]"));
   }
 
-  public void fillNewContact(ContactData contactData) {
+  public void fillContactInfo(ContactData contactData) {
     type(By.name("firstname"), contactData.getFirstName());
     type(By.name("lastname"), contactData.getLastName());
     type(By.name("nickname"), contactData.getNickname());
@@ -41,4 +41,11 @@ public class ContactHelper extends HelperBase {
     wd.switchTo().alert().accept();
   }
 
+  public void initContactModification() {
+    click(By.xpath("//*[@id=\"maintable\"]/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void submitContactModification() {
+    click(By.name("update"));
+  }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification() {
+
+    app.getContactHelper().gotoHomePage();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactInfo(new ContactData("Miles", "Morales", "Spider-Man", "Friendly Neighbor", "Avengers", "20 Ingram St.", "0987654321"));
+    app.getContactHelper().submitContactModification();
+    app.getNavigationHelper().returnToHomePage();
+  }
+
+}
