c98ba3edf1d7e3b2c74501c8a461ef141c572abd
aivanou <AIvanou@itsupportme.com>
2019-01-09 14:38:06 +0300

    GroupModificationTests creation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getGroupHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("rest1", "rest2", "rest3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getNavigationHelper().returnToGroupPage();
+
+  }
+}
