9dd81dbf0af839b5f9447deccd9395a6fc1f1f7f
aivanou <AIvanou@itsupportme.com>
2019-01-14 13:06:12 +0300

    if else text addition

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -10,7 +10,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getGroupHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("trest1", "trest2", "trest3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("trest1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getNavigationHelper().returnToGroupPage();
     app.getSessionHelper().logout();
