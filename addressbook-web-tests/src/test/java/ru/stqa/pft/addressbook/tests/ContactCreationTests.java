df0903a5853006d171dfdbf93a3a44c596274389
aivanou <AIvanou@itsupportme.com>
2019-02-12 13:05:48 +0300

    6.4 done

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,25 +1,41 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    File photo = new  File("src/test/resources/smile.png");
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withFirstName("Peter1").withLastName("Parker1").withNickname("Spider-Man1")
+            .withTitle("Friendly neighbour1").withCompany("Avengers1").withAddress("775 Westminster Avenue APT D51")
+            .withMobile("12121212121").withPhoto(photo)});
+    list.add(new Object[] {new ContactData().withFirstName("Peter2").withLastName("Parker2").withNickname("Spider-Man2")
+            .withTitle("Friendly neighbour2").withCompany("Avengers2").withAddress("775 Westminster Avenue APT D52")
+            .withMobile("12121212122").withPhoto(photo)});
+    list.add(new Object[] {new ContactData().withFirstName("Peter3").withLastName("Parker3").withNickname("Spider-Man3")
+            .withTitle("Friendly neighbour3").withCompany("Avengers3").withAddress("775 Westminster Avenue APT D53")
+            .withMobile("12121212123").withPhoto(photo)});
+    return list.iterator();
+  }
 
-  @Test
-  public void testContactCreation() throws Exception {
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    File photo = new  File("src/test/resources/smile.png");
-    ContactData contact = new ContactData().withFirstName("Peter").withLastName("Parker").withNickname("Spider-Man")
-            .withTitle("Friendly neighbour").withCompany("Avengers").withAddress("775 Westminster Avenue APT D5")
-            .withMobile("1212121212").withPhoto(photo);
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
