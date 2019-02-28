a3c56933ff397bca27144e35e1c6f9318ded1d88
aivanou <AIvanou@itsupportme.com>
2019-02-28 11:42:58 +0300

    8.5

--- Mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ Mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -24,11 +24,12 @@ public class HttpSession {
         httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
     }
 
-    public boolean login(String username, String password) throws IOException {
+    public boolean login(String username, String password, String realname) throws IOException {
         HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
         List<NameValuePair> params = new ArrayList<>();
         params.add(new BasicNameValuePair("username", username));
         params.add(new BasicNameValuePair("password", password));
+        params.add(new BasicNameValuePair("realname", realname));
         params.add(new BasicNameValuePair("secure_session", "on"));
         params.add(new BasicNameValuePair("return", "index.php"));
         post.setEntity(new UrlEncodedFormEntity(params));
--- Mantis-tests/src/test/resources/config_inc.php
+++ Mantis-tests/src/test/resources/config_inc.php
@@ -5,3 +5,6 @@ $g_database_name = 'mantis';
 $g_db_username = 'root';
 $g_db_password = 'root';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+
